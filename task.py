ae6d788a13e8a7154585a437aa5bbd2943764709
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2025-03-29 09:05:37 +0300

    v.0.0.0.3

--- task.py
+++ task.py
@@ -11,21 +11,39 @@
 # [1, 8, 0, 3, 0] -> [1, 8, 3, 0, 0]
 # [1, 8, 0, 0, 3] -> [1, 8, 3, 0, 0]
 
-arr = [1, 8, 0, 0, 3]
-l = -1
+# arr = [1, 8, 0, 0, 3]
+# l = -1
 
-for i in range(len(arr)): # O(n)
-    if arr[i] == 0 and l == -1:
-        l = i
-    elif arr[i] and l != -1:
-        arr[l] += arr[i]
-        arr[i] = 0
-        l += 1
+# for i in range(len(arr)): # O(n)
+#     if arr[i] == 0 and l == -1:
+#         l = i
+#     elif arr[i] and l != -1:
+#         arr[l] += arr[i]
+#         arr[i] = 0
+#         l += 1
 
-print(arr)
+# print(arr)
 
 # Скорость: O(n)
 # Память: O(1)
         
 
 
+import threading
+import time
+
+def task(name):
+    print(f"{name} начал работу")
+    time.sleep(2)
+    print(f"{name} завершил работу")
+
+# Создаём два потока
+t1 = threading.Thread(target=task, args=("Поток 1",))
+t2 = threading.Thread(target=task, args=("Поток 2",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+print("Все потоки завершены")
