442d9569b501d64738367eb386cbc4417ded70bb
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2025-04-12 14:43:31 +0300

    new func

---
+++ app/db_handler/db_class.py
@@ -0,0 +1,33 @@
+import sqlite3
+import datetime
+
+
+def create_connection():
+    connection = None
+    try:
+        connection = sqlite3.connect('database.db')
+        print("Подключение произошло успешно!")
+    except sqlite3.Error as e:
+        print(f"Ошибка: {e}")
+    return connection
+
+
+def create_user(user_id: int,
+                full_name: str = None):
+    conn = create_connection()
+    cursor = conn.cursor()
+    
+    find_user = """SELECT * FROM users WHERE user_id = ?"""
+    
+    cursor.execute(find_user, (user_id,))
+    user = cursor.fetchone()
+    
+    if not user:
+        create_user = """INSERT INTO users (user_id, 
+full_name, reg_date)
+        VALUES (?, ?, datetime('now'))"""
+        cursor.execute(create_user, (user_id, full_name,))
+        conn.commit()
+        return user
+
+create_user(1, "p1n0k10")
--- create_bot.py
+++ create_bot.py
@@ -6,6 +6,7 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from dotenv import load_dotenv # модуль для импорта переменных
+from app.db_handler.db_class import create_connection
 
 load_dotenv()
 
@@ -16,3 +17,23 @@ admins = [int(admin_id) for admin_id in ADMINS.split(',')]
 bot = Bot(token=BOT_TOKEN, 
           default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
+
+
+def create_tables():
+    conn = create_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS users 
+(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    full_name TEXT,
+    reg_date TEXT
+)
+    ''')
+    
+    conn.commit()
+    conn.close()
+    
+create_tables()
